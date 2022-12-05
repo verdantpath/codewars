37b578be15e34e0c9904be2188cb962c9669961a
verdantpath <m_dittmann@yahoo.com>
2022-12-04 16:09:45 -0800

    Add solution js

---
+++ lastSurvivor.js
@@ -0,0 +1,7 @@
+function lastSurvivor(letters, coords) {
+  letters = letters.split('');
+  for (let i = 0; i < coords.length; i++) {
+     letters.splice(coords[i], 1)
+  }
+  return letters.join('')
+}
