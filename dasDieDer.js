b2624b53d7de7bc5ec24844d78315746af03079c
verdantpath <m_dittmann@yahoo.com>
2022-12-12 23:02:43 -0800

    Refactor solution js

--- dasDieDer.js
+++ dasDieDer.js
@@ -1,10 +1,14 @@
 function derDieDas(wort){
-  let count = wort.match(/[aeiouäöü]/ig).length;
-  if (count < 2) {
-    return "das " + wort;
-  } else if (count > 3) {
-    return "der " + wort;
-  } else {
-    return "die " + wort;
+  let vowels = wort.match(/[aeiouäöü]/ig)
+  if (vowels) {
+    let count = vowels.length;
+     if (count < 2) {
+      return "das " + wort;
+    } else if (count > 3) {
+      return "der " + wort;
+    } else {
+      return "die " + wort;
+    }
   }
+  return "das " + wort;
 }
